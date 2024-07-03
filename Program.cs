b38f73165ae3b7794e2d7d9854427e3e48fe6746
adityaoberai <adityaoberai1@gmail.com>
2024-07-03 18:46:15 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using OpenAI;
+using OpenAI.Chat;
+using OpenAI.Images;
+
+//var openAiClient = new OpenAIClient(Environment.GetEnvironmentVariable("OPENAIKEY"));
+var azureOpenAiClient = new AzureOpenAIClient(
+    new Uri(Environment.GetEnvironmentVariable("AZUREOAIENDPOINT")),
+    new AzureKeyCredential(Environment.GetEnvironmentVariable("AZUREOAIKEY"))
+);
+
+//var chatClient = openAiClient.GetChatClient("gpt-4o");
+var chatClient = azureOpenAiClient.GetChatClient("chatgen");
+
+Console.Write("Welcome to the OpenAI .NET Chatbot.\n\nAsk me anything about tech: ");
+var input = Console.ReadLine();
+
+ChatCompletion completion = await chatClient.CompleteChatAsync(
+    [
+        new SystemChatMessage("You are a helpful tech guide that explains tech terms in simple words and less than 100 words."),
+        new UserChatMessage(input)
+    ]
+);
+
+Console.WriteLine(completion);
+
+//var imageClient = openAiClient.GetImageClient("dall-e-3");
+var imageClient = azureOpenAiClient.GetImageClient("imagegen");
+
+GeneratedImage image = await imageClient.GenerateImageAsync($"{completion}");
+
+Console.WriteLine($"Generated image: {image.ImageUri}");
